055364272f798586ed433c0d662ae6a457726975
Brian Sward <bsward@gmail.com>
2023-01-09 11:41:34 -0800

    date setup

---
+++ api/date.py
@@ -0,0 +1,16 @@
+# std lib used to handle http requests
+from http.server import BaseHTTPRequestHandler
+# datetime mod from standard lib to handle time and date
+from datetime import datetime
+
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_get(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        return
+
+
